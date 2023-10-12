af7ff6a1ada6a2ac8fc8c1d18cab367bb264f021
Adam Swenson <adam.swenson@jpmchase.com>
2023-10-12 08:56:30 -0500

    Adding dummy secrets to test hashi-cloud detecting these.

---
+++ secrets_1.py
@@ -0,0 +1,4 @@
+# Test hashicorp detecting this & performing remediation
+secret = "f7asdf72-asdf324-asdf423"
+token = "ssh-rsa asdfjiqwerjashjudfh234872385huaefhh782345h782h3t78h78fgh72h37850h74fh7834hf34"
+password = "password1"
